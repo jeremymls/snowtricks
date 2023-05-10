0d14744972b9822b379cb5a4ef9e16d243e008dd
jeremy-mls <contact@jeremy_monlouis.fr>
2023-05-10 22:47:52 +0200

    feature/48-pages-crud refactoring

--- assets/js/images.js
+++ assets/js/images.js
@@ -15,7 +15,6 @@ for (let link of links) {
             }).then(response => response.json())
             .then(data => {
                 if (data.success) {
-                    this.parentElement.parentElement.remove();
                     document.getElementById('img[' + this.dataset.id + ']').remove();
                 } else {
                     alert(data.error);
@@ -29,27 +28,39 @@ let trickImages = document.querySelector('#trick_images');
 
 trickImages.addEventListener('change', function (e) {
     let files = this.files;
-    const preview = document.querySelector('#trick_images_preview');
+    const preview = document.querySelector('#media-display');
     const thumbnail_slot = document.querySelector('#thumbnail_slot');
     const trick_miniature = document.querySelector('#trick_miniature');
     const already_add = document.querySelector('#already_add');
     const already_add_count = already_add ? already_add.childElementCount : 0;
-    preview.innerHTML = '';
+    const preview_img = document.querySelectorAll('.preview-img');
+    const separator = document.querySelector('#media-separator');
+    preview_img.forEach(image => {
+        image.remove();
+    });
 
     for (let i = 0; i < files.length; i++) {
         let file = files[i];
         let reader = new FileReader();
 
         reader.addEventListener('load', function (e) {
-            let div = document.createElement('div');
-            div.classList.add('col-6', 'col-md-3', 'col-lg-2');
+            let div_preview = document.createElement('div');
+            div_preview.classList.add('col-3', 'col-md-2', 'preview-img');
+
+            let div_preview_row = document.createElement('div');
+            div_preview_row.classList.add('row', 'preview-div', 'align-items-center');
+
+            let div_preview_col = document.createElement('div');
+            div_preview_col.classList.add('col-12');
 
             let image = document.createElement('img');
             image.src = e.target.result;
-            image.classList.add('img-fluid', 'mb-2', 'img-thumbnail');
-            div.appendChild(image);
+            image.classList.add('img-fluid');
 
-            preview.appendChild(div);
+            div_preview_col.appendChild(image);
+            div_preview_row.appendChild(div_preview_col);
+            div_preview.appendChild(div_preview_row);
+            preview.insertBefore(div_preview, separator);
         });
 
         reader.readAsDataURL(file);
